01a1ac364d7f5c80c8059d99a3f35738bff08e8a
Jyotirmay Sarna <jyotirmay@kali3.internal.systematicdefence.tech>
2026-06-21 22:32:30 +1200

    See Readme.md Kali Linux version 0.15 hotfix 5

--- terraform/aks.tf
+++ terraform/aks.tf
@@ -42,10 +42,13 @@ resource "azurerm_kubernetes_cluster" "aks" {
 #}
 
 resource "azurerm_role_assignment" "aks_acr_pull" {
+  # ADD THIS LINE: Explicitly locks the GUID so it never changes or conflicts
+  name                             = uuidv5("url", "https://jaydemo.azure")
   # References your existing Azure Container Registry resource ID
   scope                = azurerm_container_registry.acr.id # Change "acr" to match your actual ACR local name
   role_definition_name = "AcrPull"
   
   # FIXED: Target the Kubelet Identity block instead of the Control Plane Identity
   principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  skip_service_principal_aad_check = true
 }
