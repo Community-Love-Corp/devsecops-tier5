20e187d47e178156c2e9df1030ac2257ba393952
Jyotirmay Sarna <jyotirmay@kali3.internal.systematicdefence.tech>
2026-06-22 22:02:10 +1200

    See Readme.md Kali Linux version 0.18 Hotfix #5 try 16

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,17 +10,17 @@ variable "location" {
 
 variable "csi_identity_resource_id" {
   type = string
-  default = null
+  default = "/subscriptions/eac7dbb7-a7ec-45bc-98de-902b34a2f404/resourcegroups/MC_jaydemo-rg_jaydemo-aks_australiaeast/providers/Microsoft.ManagedIdentity/userAssignedIdentities/azurekeyvaultsecretsprovider-jaydemo-aks"
 }
 
 variable "csi_identity_client_id" {
   type = string
-  default = null
+  default = "a506e50a-a0aa-4e1a-8c73-eec589c280a0"
 
 }
 
 variable "csi_identity_object_id" {
   type = string
-  default = null
+  default = "60d54a16-dbaa-4ccf-8436-a465e259c6c5"
 
 }
